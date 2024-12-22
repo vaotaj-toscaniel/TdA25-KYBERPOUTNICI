e110818c03a63c1a2679d89c4e4bee9f07c560ef
Vojtěch Adam <lordofotaj@gmail.com>
2024-12-22 17:31:58 +0100

    Updated to Nuxt4 structure
    +Added api.yaml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,23 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+
+  future: {
+    compatibilityVersion: 4,
+  },
+
+  experimental: {
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    defaults: {
+      useAsyncData: {
+        deep: true
+      }
+    }
+  },
   devtools: { enabled: true },
-  pages: true
+  
 })
