a9cffdf4d8da5c63f3de3c64f0a154eb6fee4334
Martin140K <martinkubousek14@gmail.com>
2025-01-14 20:55:21 +0100

    Update cors.ts

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -1,7 +1,12 @@
 import { H3Event } from 'h3';
 
 export default defineEventHandler((event: H3Event) => {
-  event.node.res.setHeader('Access-Control-Allow-Origin', '*'); // Umožní všechny domény přístup
-  event.node.res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  event.node.res.setHeader('Access-Control-Allow-Origin', '*');
+  event.node.res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   event.node.res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (event.node.req.method === 'OPTIONS') {
+    event.node.res.statusCode = 204;
+    event.node.res.end();
+  }
 });
